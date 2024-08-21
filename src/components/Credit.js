1bb3bbf64f5a6dac400288a65b29a146ba35ba4c
babosaji123 <151328131+babosaji123@users.noreply.github.com>
2024-08-21 23:48:40 +0530

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>Credit:
+      <br></br>
+      @VerveBridge Internship
+    </p>
+      {/* <img src="https://media.licdn.com/dms/image/v2/D560BAQFx0p8KWD1xjg/company-logo_200_200/company-logo_200_200/0/1715528250608/vervebridge_logo?e=2147483647&v=beta&t=vlTd8dGwhHM-_payr9GiHhvLCFBUwVPhK1DHgFV4l8Y"></img> */}
+      
+    
+  </div>
+  );
+}
